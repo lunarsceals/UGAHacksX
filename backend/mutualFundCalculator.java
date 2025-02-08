af125b080ce8729c730580570bb70f86ba1b2073
BryantMonahan <129926467+BryantMonahan@users.noreply.github.com>
2025-02-07 23:13:54 -0500

    Update mutualFundCalculator.java
    added function to include depreciation

--- backend/mutualFundCalculator.java
+++ backend/mutualFundCalculator.java
@@ -8,4 +8,14 @@ public class mutualFunCalculator {
         }
         return values;
     }
+
+    public static double[] createArrayOfWorthWithDepreciation(long apr, int years, double principal, double monthlyContribution, long inflation){
+        double[] values = new double[years*12];
+        values[0] = principal;
+
+        for(int i = 1; i < years*12; i++){
+            values[i] = values[i-1] + monthlyContribution + (values[i-1] * (apr/12/100 - inflation/12/100));
+        }
+        return values;
+    }
 }
