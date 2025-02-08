45c38c560452250de3bcee3acf8f0167d6a1cfe2
BryantMonahan <129926467+BryantMonahan@users.noreply.github.com>
2025-02-08 18:50:10 -0500

    Update InvestmentService.java

--- backend2/src/main/java/com/example/demo/backend/InvestmentService.java
+++ backend2/src/main/java/com/example/demo/backend/InvestmentService.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InvestmentService {
+
+        // This creates an array of monthlyValue objects that store the values of the investment.
+    public static monthlyValue[] calculateMonthlyValues(long apr, long depreciatedValue, int years, double principal, double monthlyContribution){
+        monthlyValue[] values = new monthlyValue[years*12+1];
+        values[0] = new monthlyValue(principal, principal, principal,0);
+        for(int i = 1; i < years*12+1; i++){
+            values[i] = new monthlyValue(values[i-1].totalPrincipal + monthlyContribution, values[i-1].totalValue + monthlyContribution + (values[i-1].totalValue * apr/12/100), values[i-1].totalValue + monthlyContribution + (values[i-1].totalValue * apr-depreciatedValue/12/100), i);
+        }
+        return values;
+    }
     
     public double[] createArrayOfWorth(long apr, int years, double principal, double monthlyContribution) {
         double[] values = new double[years * 12];
