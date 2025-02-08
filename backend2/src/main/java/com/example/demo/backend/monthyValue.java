ddaefeabd16a229b4a4c7358b768ee9eb6b3b224
BryantMonahan <129926467+BryantMonahan@users.noreply.github.com>
2025-02-08 18:49:06 -0500

    Create monthyValue.java

---
+++ backend2/src/main/java/com/example/demo/backend/monthyValue.java
@@ -0,0 +1,25 @@
+// This class is used to store the totalPrincipal, totalInterest, totalValue, and the 
+// month of growth (I.E. the 11th month of growth). This should make it easier to store
+// data we plan to chart and send it to front end as JSON objects.
+public class monthlyValue {
+    double totalPrincipal;
+    double totalValue;
+    double depreciatedValue;
+    int month;
+
+    // Creates on object with depreciated values stored
+    public monthlyValue(double totalPrincipal, double totalValue, double depreciatedValue, int month){
+        this.month = month;
+        this.totalPrincipal = totalPrincipal;
+        this.totalValue = totalValue;
+        this.depreciatedValue = depreciatedValue;
+    }
+
+    // Creates an object without depreciated values stored
+    public monthlyValue(double totalPrincipal, double totalValue, long apr, int month){
+        this.month = month;
+        this.totalPrincipal = totalPrincipal;
+        this.totalValue = totalValue;
+    }
+    
+}
