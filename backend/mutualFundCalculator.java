f80c19e2c5a06d0a44788380824d9eebaf25bc2b
BryantMonahan <129926467+BryantMonahan@users.noreply.github.com>
2025-02-07 22:54:51 -0500

    Update mutualFundCalculator.java
    added method

---
+++ backend/mutualFundCalculator.java
@@ -0,0 +1,11 @@
+public class mutualFunCalculator {
+    public static double[] createArrayOfWorth(long apr, int years, double principal, double monthlyContribution){
+        double[] values = new double[years*12];
+        values[0] = principal;
+
+        for(int i = 1; i < years*12; i++){
+            values[i] = values[i-1] + monthlyContribution + (values[i-1] * apr/12/100);
+        }
+        return values;
+    }
+}
